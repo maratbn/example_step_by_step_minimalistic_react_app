c636c0efbec34a743aa10017e886705973718c49
Marat Nepomnyashy <maratbn@gmail.com>
2017-01-15 22:11:13 -0800

    Added 'webpack_in/entry.js' that contains the initial JavaScript entry logic.

---
+++ webpack_in/entry.js
@@ -0,0 +1,6 @@
+//  This file 'entry.js' is part of an example for building a minimalistic React front-end app step
+//  by step as outlined in the tutorial blog at
+//  http://maratbn.com/blogs/2017/01/14/minimalistic-react-webapp/
+
+
+console.log("JavaScript entry logic.");
