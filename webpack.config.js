400ee13e7a5fc28b6ce48409a5daabaf3f35f9c3
Marat Nepomnyashy <maratbn@gmail.com>
2017-01-15 22:26:07 -0800

    Added 'webpack.config.js' with just the initial Webpack configuration to pack 'webpack_in/entry.js' into 'webpack_out/minimalistic_react.js'.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+//  This file 'webpack.config.js' is part of an example for building a minimalistic React front-end
+//  app step by step as outlined in the tutorial blog at
+//  http://maratbn.com/blogs/2017/01/14/minimalistic-react-webapp/
+
+
+const path = require('path');
+
+module.exports = {
+    entry: [path.join(__dirname, 'webpack_in', 'entry.js')],
+    output: {
+        path:      path.join(__dirname, 'webpack_out'),
+        filename:  'minimalistic_react.js'
+      }
+  };
