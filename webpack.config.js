1a221ffca9b249403148c444b4f28cc53845ff5a
Marat Nepomnyashy <maratbn@gmail.com>
2018-06-29 22:36:19 -0700

    Changed the boilerplate content from the previous minimalistic react app tutorial to now refer to the new tutorial for building a multi-widget React app.  This is being forked-off the branch 'webpack4--preact--package.json'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-//  This file 'webpack.config.js' is part of an example for building a minimalistic React front-end
-//  app step by step with Webpack 4 as outlined in the tutorial blog at
-//  http://maratbn.com/blogs/2018/06/25/minimalistic-react-webapp-webpack-4/
+//  This file 'webpack.config.js' is part of an example for building a multi-widget React front-end
+//  app step by step as outlined in the tutorial blog at
+//  http://maratbn.com/blogs/2018/07/02/react-multi-widget/
 
 
 const path = require('path');
@@ -27,7 +27,7 @@ module.exports = {
       },
     output: {
         path:      path.join(__dirname, 'webpack_out'),
-        filename:  'minimalistic_react.js'
+        filename:  'main.js'
       },
     mode: 'development'
   };
--- webpack_in/entry.jsx
+++ webpack_in/entry.jsx
@@ -1,6 +1,6 @@
-//  This file 'entry.jsx' is part of an example for building a minimalistic React front-end app
-//  step by step with Webpack 4 as outlined in the tutorial blog at
-//  http://maratbn.com/blogs/2018/06/25/minimalistic-react-webapp-webpack-4/
+//  This file 'entry.jsx' is part of an example for building a multi-widget React front-end app
+//  step by step as outlined in the tutorial blog at
+//  http://maratbn.com/blogs/2018/07/02/react-multi-widget/
 
 
 console.log("JSX entry logic.");
