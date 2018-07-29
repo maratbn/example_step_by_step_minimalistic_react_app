371bed7b13a43ca2ff6b3ae1c82eefb590e93f0e
Marat Nepomnyashy <maratbn@gmail.com>
2018-07-28 21:49:40 -0700

    Un-commented the aliasing / substitution of Preact for React.

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = {
       },
     resolve: {
         alias: {
-            //'react':      'preact-compat',
-            //'react-dom':  'preact-compat'
+            'react':      'preact-compat',
+            'react-dom':  'preact-compat'
           }
       },
     output: {
